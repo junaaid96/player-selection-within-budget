7ae1da90bd0decf098f388e82ef0dc314c847a06
junaaid96 <info.junaidulislam@gmail.com>
2022-08-22 23:02:18 +0600

    total calculation added

--- js/budget.js
+++ js/budget.js
@@ -2,11 +2,25 @@ const playerCost = document.getElementById('player-cost');
 const calculateBtn = document.getElementById('calculate-btn');
 const playerExpenses = document.getElementById('player-expenses');
 const playerLists = document.getElementById('player-list').getElementsByTagName('li');
+const managerCost = document.getElementById('manager-cost');
+const coachCost = document.getElementById('coach-cost');
+const totalCost = document.getElementById('total-cost');
 
 playerCost.addEventListener('keyup', function (event) {
-    const playerCostField = event.target.value;
+    const playerCostField = parseInt(event.target.value);
     calculateBtn.addEventListener('click', function () {
-        playerExpenses.innerText = playerCostField * playerLists.length;
+        const totalPlayerExpenses = playerCostField * playerLists.length;
+        playerExpenses.innerText = totalPlayerExpenses;
+        managerCost.addEventListener('keyup',function(event){
+            const managerCostField = parseInt(event.target.value);
+            coachCost.addEventListener('keyup', function(event){
+                const coachCostField = parseInt(event.target.value);
+                document.getElementById('total-btn').addEventListener('click', function(){
+                    totalCost.innerText = totalPlayerExpenses + managerCostField + coachCostField;
+                })
+            })
+        })
+        
     })
+    
 })
-
