9b5db45395f566d0582595b872fe9b9bd6d38f1e
junaaid96 <info.junaidulislam@gmail.com>
2022-08-23 22:09:39 +0600

    updated budget calculation

--- js/budget.js
+++ js/budget.js
@@ -6,20 +6,18 @@ const managerCost = document.getElementById('manager-cost');
 const coachCost = document.getElementById('coach-cost');
 const totalCost = document.getElementById('total-cost');
 
+const managerCostField = parseInt(managerCost.value);
+const coachCostField = parseInt(coachCost.value);
+
 playerCost.addEventListener('keyup', function (event) {
     const playerCostField = parseInt(event.target.value);
     calculateBtn.addEventListener('click', function () {
         const totalPlayerExpenses = playerCostField * playerList.length;
         playerExpenses.innerText = totalPlayerExpenses;
         event.stopPropagation;
-        managerCost.addEventListener('keyup', function (event) {
-            const managerCostField = parseInt(event.target.value);
-            coachCost.addEventListener('keyup', function (event) {
-                const coachCostField = parseInt(event.target.value);
-                document.getElementById('total-btn').addEventListener('click', function () {
-                    totalCost.innerText = totalPlayerExpenses + managerCostField + coachCostField;
-                })
-            })
+        document.getElementById('total-btn').addEventListener('click', function () {
+            const inTotal = totalPlayerExpenses + managerCostField + coachCostField;
+            totalCost.innerText = inTotal;
         })
     })
 })
